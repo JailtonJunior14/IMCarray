3510d40ae87af18ad1a8aae3251a98066d5d15c1
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-04-30 12:23:10 -0300

    Adicionar arquivos de projeto.

---
+++ IMCarray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMCarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[5];
+            double[] peso = new double[5];
+            double[] altura = new double[5];
+            double[] imc = new double[5];
+            for (int i = 0; i <= 4; i++) 
+            {
+                Console.WriteLine("Insira seu nome: ");
+
+            }
+
+        }
+    }
+}
